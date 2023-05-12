dad338350efe796395f780ba54e64a8f563aee10
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-05-12 17:06:31 +0200

    add signIn and signOut

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from "../styles";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Card from "@/components/Card";
 import { db } from "../db/db";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function HomePage() {
+  const { data: session } = useSession();
   return (
     <div>
       <h1>Welcome to my plattenladen</h1>
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
       <ul>
-      {
-        db.map(entry=>(
-          <Card key={entry.id} article={entry}/>
-        ))
-      }
+        {db.map((entry) => (
+          <Card key={entry.id} article={entry} />
+        ))}
       </ul>
     </div>
   );
